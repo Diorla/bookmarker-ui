33f028e391aaeb1734378cfbf4195cf12c2467eb
Ade Adeola <adedotster@gmail.com>
2022-10-04 12:05:21 +0100

    update readme

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import Button from './button';
 import ThemeProvider from './ThemeProvider';
 import ThemeProps from './interfaces/ThemeProps';
+import theme from './theme';
 
-export { Button, ThemeProvider, ThemeProps };
+export { Button, ThemeProvider, ThemeProps, theme };
